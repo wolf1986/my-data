5f0be95d1bcce9684787942efaf8c8997388691f
Willy Fenchenko <wolf1986@gmail.com>
2018-01-21 18:59:01 +0200

    Added: Google user authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { OnDestroy, AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Subscription } from 'rxjs/Subscription';
+import { GoogleAuthService } from './google-auth.service';
+
+export let Config = {
+  GoogleAuth: {
+    ClientId: '442618660562-7nlckakrfi4gmtgqg1bnjkganvc28fu8.apps.googleusercontent.com',
+    Scopes: [
+      'https://www.googleapis.com/auth/spreadsheets',
+      'https://www.googleapis.com/auth/drive.metadata.readonly',
+      'https://www.googleapis.com/auth/drive',
+    ],
+  },
+  GoogleApiDiscoveryDocs: [
+    'https://sheets.googleapis.com/$discovery/rest?version=v4',
+    'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest',
+  ],
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnDestroy, AfterViewInit {
   title = 'app';
+  subscriptionGoogleUser: Subscription;
+
+  constructor(private googleAuthService: GoogleAuthService) {
+    this.subscriptionGoogleUser = this.googleAuthService.googleUser$.subscribe(this.onGoogleUser);
+  }
+
+  onGoogleUser(googleUser: gapi.auth2.GoogleUser) {
+    console.log(googleUser);
+  }
+
+  ngAfterViewInit() {
+    this.googleAuthService.clientConfig = {
+      client_id: Config.GoogleAuth.ClientId,
+      scope: Config.GoogleAuth.Scopes.join(' '),
+    };
+
+    this.googleAuthService.initAuth2Flow();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionGoogleUser.unsubscribe();
+  }
 }
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { GoogleAuthService } from '../google-auth.service';
+import { Subscription } from 'rxjs/Subscription';
+
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent implements OnInit {
 
-  constructor() { }
+export class SettingsComponent implements OnInit, AfterViewInit {
+  fields: any;
+
+  constructor(public googleAuthService: GoogleAuthService) {
+    this.fields = {};
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.googleAuthService.promiseInitCompleted.then(() => {
+      const auth2 = gapi.auth2.getAuthInstance();
+      auth2.attachClickHandler(
+        document.getElementById('googleBtn'), {}, null, null);
+    });
+  }
+
+  public onSignOutClicked() {
+    const auth2 = gapi.auth2.getAuthInstance().signOut();
+  }
 }
